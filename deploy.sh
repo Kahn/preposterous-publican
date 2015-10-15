8e4005058c4cadf8fc63ea0d2a39543ba8de89a2
Sam Wilson <swilsonau@gmail.com>
2015-10-15 21:58:15 +1100

    Fix builds

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,4 @@ git config user.email "noreply@example.org"
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+exit 0
